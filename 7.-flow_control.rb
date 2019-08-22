e6ebf481e4019bf517f74d85d8ac78264efcc7d9
Jonathan <join_mg@hotmail.com>
2019-08-22 09:08:17 -0500

    Exercises ruby :)

--- 7.-flow_control.rb
+++ 7.-flow_control.rb
@@ -47,3 +47,14 @@ end
 # and 2004). However, years divisible by 100 are not leap years
 # (such as 1800 and 1900) unless they are also divisible by 400
 # (such as 1600 and 2000, which were in fact leap years).
+puts ''
+puts 'Leap years'
+puts 'Enter starting year:'
+start_year = gets.chomp
+puts 'Enter ending year'
+ending_year = gets.chomp
+while start_year.to_i <= ending_year.to_i && start_year = start_year.to_i + 1
+  if(start_year % 4 == 0) && (start_year % 100 != 0) || (start_year % 400 == 0)
+    puts start_year.to_s + ' is a leap year'
+  end
+end
