c4463e7bc4cad354eed60c7836bdc7766b8f9095
Jonathan <join_mg@hotmail.com>
2019-08-27 12:05:17 -0500

    5.1 - Person's name exercise

---
+++ 5.1.-Persons_name.rb
@@ -0,0 +1,11 @@
+# Problem 1
+# Write a program that ask for a person's frist name, then middle, and then last
+# Finally, it should freet the person using their full name.
+
+puts 'Hello friend, what\'s your name?'
+first_name = gets
+puts 'Awesome, what\'s your middle name?'
+middle_name = gets
+puts 'And what\'s your last name?'
+last_name = gets
+puts 'Pleased to meet you, ' + first_name + middle_name + last_name + '. :)'
