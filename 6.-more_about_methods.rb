3260b6af5d4ca14f41d18af53d028e281623e874
Jonathan <join_mg@hotmail.com>
2019-08-27 12:06:24 -0500

    Chapter 6 - More about methods

--- 6.-more_about_methods.rb
+++ 6.-more_about_methods.rb
@@ -1,5 +1,4 @@
 # More about methods
-
 # reverse
 msg = 'OMG this msg will reversed'
 puts 'Reverse: ' + msg.reverse
@@ -42,38 +41,7 @@ puts(str.center(line_width2))
 puts(str.rjust(line_width2))
 puts(str.ljust(line_width2/2) + str.ljust(line_width2/2))
 
-# a few thngs to try
-# Angry boss. Write an angry boss program that rudely asks what you want.
-# Whatever you answer, the angry boss should yell it back to you and then
-# fire you. For example, if you type in I want a raise, it should yell back like
-# this:
-# WHADDAYA MEAN "I WANT A RAISE"?!? YOU'RE FIRED!!
-puts 'What you want?'
-answer = gets
-puts 'WHADDAYA MEAN ' + answer.to_s + '?!? YOU\'RE FIRED!!'
-puts ''
-
-# Table of contents. Here's something for you to do in order to play around
-# more with center, ljust, and rjust: write a program that will displays a table
-# of contents so that it looks like this:
-#               Table of Contents
-# Chapter 1: Getting Started                    page 1
-# Chapter 2: Numbers                            page 9
-# Chapter 3: Letters                            page 13
-line_width3 = 60
-title = 'Table of Contents'
-chapter_1_text = 'Chapter 1: Getting Started'
-chapter_1_page = 'page 1'
-chapter_2_text = 'Chapter  2: Numbers'
-chapter_2_page = 'page 9'
-chapter_3_text = 'Chapter 3: Letters'
-chapter_3_page = 'page 13'
-puts(title.center(line_width3))
-puts(chapter_1_text.ljust(line_width3) + chapter_1_page.ljust(line_width3))
-puts(chapter_2_text.ljust(line_width3) + chapter_2_page.ljust(line_width3))
-puts(chapter_3_text.ljust(line_width3) + chapter_3_page.ljust(line_width3))
-
 # Random numbers
-puts ''
+puts
 puts 'Random number from 1 to 100:'
 puts(rand(100))
