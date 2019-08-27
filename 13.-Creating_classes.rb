f479923b31d52ec2f83355dd3329b996de3c46ea
Jonathan <join_mg@hotmail.com>
2019-08-27 11:26:23 -0500

    Chapter 13 - Creating classes

--- 13.-Creating_classes.rb
+++ 13.-Creating_classes.rb
@@ -1,1 +1,37 @@
 # Creating Classes
+# Example 1
+class Die
+  def roll
+    1 + rand(6)
+  end
+end
+
+# Let's make a couple of dice...
+dice = [Die.new, Die.new]
+
+# ...and roll them
+puts 'Example 1 - Creting Classes'
+dice.each do |die|
+  puts die.roll
+end
+
+# Instance Variables
+# Example 2
+class Die
+  def roll
+    @number_showing = 1 + rand(6)
+  end
+  def showing
+    @number_showing
+  end
+end
+
+puts
+puts 'Example 2 - Instance variables'
+die = Die.new
+die.roll
+puts die.showing
+puts die.showing
+die.roll
+puts die.showing
+puts die.showing
